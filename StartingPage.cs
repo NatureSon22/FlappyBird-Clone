a84db767fad3adab228857d85306e30fd6bf8541
Nature Son <110081044+NatureSon22@users.noreply.github.com>
2023-01-29 13:46:10 +0700

    Add files via upload

---
+++ StartingPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBirdClone
+{
+    public partial class StartingPage : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+        public StartingPage()
+        {
+            InitializeComponent();
+            Button button = new Button();
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Game game= new Game();
+            game.Size = new Size(550, 550);
+            game.Show();
+            this.Hide();
+        }
+
+        private void guna2PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
